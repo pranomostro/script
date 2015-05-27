191f24c3cbbc2f7c5aebf0f5b705b04f324fd4f8
adrian <a.regenfuss@gmx.de>
2015-05-28 00:34:33 +0200

    Wrote a man-page and rewrote the source.

--- chan.sh
+++ chan.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-for f in `ls | grep "$1"`; do
-	echo -e "H\n"",s/$2/$3/g\n"",w" | ed "$f"
+
+if [ $# -le 1 -o $# -ge 4 ]; then
+	echo "chan [FILES] FROM TO"
+	exit 1
+fi
+
+if [ $# -eq 2 ]; then ptrn=""; from="$1"; to="$2";
+else ptrn="$1"; from="$2"; to="$3"; fi
+
+for f in `ls | grep "$ptrn"`; do
+	echo -e "H\n,s/$from/$to/g\n,wq" | ed "$f"
 done
 
+exit 0
