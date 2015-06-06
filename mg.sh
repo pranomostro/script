1de7b11bd24290d670aae4ded3c07f3138113789
adrian <a.regenfuss@gmx.de>
2015-06-06 12:41:07 +0200

    ADded an error-message for mg.

--- mg.sh
+++ mg.sh
@@ -9,5 +9,7 @@ c=($(gen $b))
 b=`expr $b - 1`
 
 for d in `seq 0 $b`;do
-	mv ${a[$d]} ${c[$d]}
+	if [ -e "${c[$d]}" ]; then echo "error: not replacing ${c[$d]} with ${a[$d]}" >/dev/stderr; exit 1; fi
+
+	mv "${a[$d]}" "${c[$d]}"
 done
