10de9abb1d8323cda0ddee50eec4563348181cfb
adrian <a.regenfuss@gmx.de>
2015-06-11 17:50:23 +0200

    Wrote the man-page.

--- nt.sh
+++ nt.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+a=`pwd`
+
 for i in `find "$1" -type l -print`; do
 
 	j=`basename "$i"`
-	k=`sed "s/${j}$//"`
+	k=`echo "$i" | sed "s/$j$//"`
+
 	cd "$k"
-	l=`ls -l $i | awk '{ print $11 }'`
+
+	l=`ls -l "$j" | awk '{ print $11 }'`
 
 	if [ ! -e "$l" ]; then
 		echo "$i"
 	fi
+
+	cd "$a"
 done
