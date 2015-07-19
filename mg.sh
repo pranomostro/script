feb9a28e5ddd8db7dab641ad0ed8a37e45363b4c
adrian <a.regenfuss@gmx.de>
2015-07-20 00:30:16 +0200

    Shortened lines to 80 characters maximum

--- mg.sh
+++ mg.sh
@@ -9,7 +9,10 @@ c=($(gen $b))
 b=`expr $b - 1`
 
 for d in `seq 0 $b`;do
-	if [ -e "${c[$d]}" ]; then echo "error: not replacing ${c[$d]} with ${a[$d]}" >/dev/stderr; exit 1; fi
+	if [ -e "${c[$d]}" ]; then
+		echo "error: not replacing ${c[$d]} with ${a[$d]}" >/dev/stderr;
+		exit 1;
+	fi
 
 	mv "${a[$d]}" "${c[$d]}"
 done
