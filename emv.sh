c5c8aec809607ec21814030c55c13f9a12cd944f
adrian <a.regenfuss@gmx.de>
2015-07-19 23:46:35 +0200

    Added option '-t' to emv.sh.

--- emv.sh
+++ emv.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-if [ $# -le 1 -o $# -ge 3 ]; then
-	echo "emv FROM TO"
+if [ $# -le 1 -o $# -ge 4 ]; then
+	echo "emv [-t] FROM TO"
 fi
 
-for i in `ls | grep "$1"`; do
-	j=`echo "$i" | sed "s/$1/$2/"`
+if [ $# -eq 2]; then
+	from="$1"; to="$2";
+else
+	if [ $1 != '-t' ]; then
+		echo "error: flag $1 not recognized, exiting" >/dev/stderr; exit 2;
+	else
+	trust="1"; from="$2"; to="$3"
+fi
 
-	if [ -e "$j" ]; then echo "error: not replacing $j with $i, exiting" >/dev/stderr; exit 2; fi
+for i in `ls | grep "$from"`; do
+	j=`echo "$i" | sed "s/$from/$to/"`
+
+	if [ -e "$j" -a $trust != "1" ]; then echo "error: not replacing $j with $i, exiting" >/dev/stderr; exit 3; fi
 	mv $i $j
 done
