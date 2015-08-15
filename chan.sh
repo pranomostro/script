721ab8524376f349367ada4aab95e5843fe40ead
scharlatan <a.regenfuss@gmx.de>
2015-08-15 23:24:32 +0200

    Changed the style for better.

--- chan.sh
+++ chan.sh
@@ -5,10 +5,12 @@ if [ $# -le 1 -o $# -ge 4 ]; then
 	exit 1
 fi
 
+set -eruo pipefail
+
 if [ $# -eq 2 ]; then ptrn=""; from="$1"; to="$2";
 else ptrn="$1"; from="$2"; to="$3"; fi
 
-for f in `ls | grep "$ptrn"`; do
+for f in $(ls | grep "$ptrn"); do
 	echo -e "H\n,s/$from/$to/g\n,wq" | ed "$f"
 done
 
