f522e71a792d1df0096248877a0ba83e601912c3
scharlatan <a.regenfuss@gmx.de>
2015-08-16 23:33:00 +0200

    Added old sh version of chan.

---
+++ chan.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -eruo pipefail
+
+if [ $# -le 1 -o $# -ge 4 ]; then
+	echo "chan [FILES] FROM TO" >/dev/stderr
+	exit 1
+fi
+
+if [ $# -eq 2 ]; then ptrn=""; from="$1"; to="$2";
+else ptrn="$1"; from="$2"; to="$3"; fi
+
+for f in $(ls | grep "$ptrn"); do
+	echo -e "H\n,s/$from/$to/g\n,wq" | ed "$f"
+done
+
+exit 0
