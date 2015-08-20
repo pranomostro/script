638145b15baaaa03b8050ee54ac5445483738746
scharlatan <a.regenfuss@gmx.de>
2015-08-20 22:36:42 +0200

    Wrote rc versions of ecp and emv.

--- ecp.sh
+++ ecp.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 if [ $# -le 1 -o $# -ge 4 ]; then
 	echo "ecp [-t] FROM TO"
+	exit 1
 fi
 
 if [ $# -eq 2 ]; then
 	from="$1"; to="$2";
 else
-	if [ $1 != '-t' ]; then
+	if [ "$1" != '-t' ]; then
 		echo "error: flag $1 not recognized, exiting" >/dev/stderr; exit 2;
 	fi
 	trust="1"; from="$2"; to="$3"
@@ -18,5 +19,5 @@ for i in `ls | grep "$from"`; do
 	if [ -e "$j" -a "$trust" != "1" ]; then
 		echo "error: not replacing $j with $i, exiting" >/dev/stderr; exit 3;
 	fi
-	cp $i $j
+	cp "$i" "$j"
 done
--- emv.sh
+++ emv.sh
@@ -6,7 +6,7 @@ fi
 if [ $# -eq 2 ]; then
 	from="$1"; to="$2";
 else
-	if [ $1 != '-t' ]; then
+	if [ "$1" != '-t' ]; then
 		echo "error: flag $1 not recognized, exiting" >/dev/stderr; exit 2;
 	fi
 	trust="1"; from="$2"; to="$3"
@@ -18,5 +18,5 @@ for i in `ls | grep "$from"`; do
 	if [ -e "$j" -a "$trust" != "1" ]; then
 		echo "error: not replacing $j with $i, exiting" >/dev/stderr; exit 3;
 	fi
-	mv $i $j
+	mv "$i" "$j"
 done
