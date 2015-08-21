e3f12849cb95c3626972c33c6a358b2182c0d51e
scharlatan <a.regenfuss@gmx.de>
2015-08-21 23:16:24 +0200

    Wrote an updated sh version.

--- chan.sh
+++ chan.sh
@@ -2,16 +2,13 @@
 
 set -eruo pipefail
 
-if [ $# -le 1 -o $# -ge 4 ]; then
-	echo "chan [FILES] FROM TO" >/dev/stderr
-	exit 1
-fi
+case $# in
+1|2)	echo 'chan FROM TO FILES' 1>&2; exit 1
+*)	from="$1"; to="$2"; shift 2
+esac
 
-if [ $# -eq 2 ]; then ptrn=""; from="$1"; to="$2";
-else ptrn="$1"; from="$2"; to="$3"; fi
-
-for f in $(ls | grep "$ptrn"); do
-	echo -e "H\n,s/$from/$to/g\n,wq" | ed "$f"
+for file in $@; do
+	echo -e 'H\n ,s/'"$from"'/'"$to"'/g\n wq' | ed "$file"
 done
 
 exit 0
