ee52e33994bb1ca869a13e6e95b214df2df873f7
scharlatan <a.regenfuss@gmx.de>
2015-09-06 17:57:08 +0200

    Simplified argument handling.

--- chan.sh
+++ chan.sh
@@ -3,11 +3,11 @@
 set -eruo pipefail
 
 case $# in
-1|2)	echo 'chan FROM TO FILES' 1>&2; exit 1
+0|1|2)	echo 'chan FROM TO FILES' 1>&2; exit 1
 *)	from="$1"; to="$2"; shift 2
 esac
 
-for file in $@; do
+for file; do
 	echo -e 'H\n ,s/'"$from"'/'"$to"'/g\n wq' | ed "$file"
 done
 
