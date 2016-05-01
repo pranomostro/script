e6096289b79357f18b89392e7e557e91d1a9288c
scharlatan <a.regenfuss@gmx.de>
2016-05-01 16:36:43 +0200

    Added tlc to the makefile and fixed argument handling.

--- config.mk
+++ config.mk
@@ -2,4 +2,4 @@
 
 PREFIX=/usr/local
 
-TARGET=4img 9 bns bomb bu chv ell empty fds field gm lg musconv musmenu must opus2flac pfm play pprz r rip sal sts tb barign barinfo beau big cnt wsch
+TARGET=4img 9 bns bomb bu chv ell empty fds field gm lg musconv musmenu must opus2flac pfm play pprz r rip sal sts tb tlc barign barinfo beau big cnt wsch
