ae1dbb94edb15b05ae845587c2a09bf39856d893
pranomostro <pranomestro@gmail.com>
2016-10-03 20:10:02 +0200

    Removed must, it ain't gonna happen :(. But we have scc!

--- config.mk
+++ config.mk
@@ -1,5 +1,6 @@
+
 #yay, configuration variables!
 
 PREFIX=/usr/local
 
-TARGET=4img 9 barign barinfo beau big bns bomb bu chv cnt cts dec ell empty enc fds field gm lg musconv musmenu must opus2flac pfm play pprz r rip sal shp sts tb tlc wsch zug
+TARGET=4img 9 barign barinfo beau big bns bomb bu chv cnt cts dec ell empty enc fds field gm lg musconv musmenu opus2flac pfm play pprz r rip sal shp sts tb tlc wsch zug
